666feb2ec4a98c1c8096192ab6485b0bad15367c
Haroon Awan <haroonlajpal123@gmail.com>
2026-07-08 00:21:01 -0700

    reset updation 8

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,9 +4,7 @@ const sendEmail = async (options) =>{
     console.log("Email pass exisit",!!process.env.EMAIL_PASS);
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port:465,
-        secure:false,
-        family:4,
+        service: "gmail",
         auth: {
             user: process.env.EMAIL_USER,
             pass:process.env.EMAIL_PASS,
